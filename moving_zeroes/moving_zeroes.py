fcf9101aa006a765633e91e70fa3a1d2b33de0cf
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 15:46:53 -0400

    finished single_number

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,14 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    # shift 0's to the left side of the array
+    # return the altered array
+    # if there are no 0s print the array
+    # create a variable to hold the non-zeroes
+    # Loop - for x in the range of the length of the array
+        # x holds the zeroes in the equation, so if x doesnt equal zero and moved DOES equal zero
+        # the array's order becomes the moved nonzero number to the left, and the zero to the right for the entire array
+        # if moved didn't equal zero, move the index to check the next one
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,33 +2,30 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
 def single_number(arr):
-    # Your code here
-    # Need to check an array for number that only occurs once while the rest show up twice
-    # Does array actually have duplicates? use set()??
-    # How to look through an array to check if there is an integer that does NOT show up twice(loop??)
-    #If a single itnerger is in the array return that integer into a new array?? 
-    #Else continue looping through the array until it is found
+     # Your code here
+#     # Need to check an array for number that only occurs once while the rest show up twice
+#     # Does array actually have duplicates? use set()??
+#     # How to look through an array to check if there is an integer that does NOT show up twice(loop??)
+#     #If a single itnerger is in the array return that integer into a new array?? 
+#     #Else continue looping through the array until it is found
 
-
-################
-    #size = len(arr)
-    #create a new array to find x
+    #create a new array for the not duplicate number to go in
     not_dupl = []
-    #loop through array
+    #loop through array to find x
     for x in arr:
-        #check if x should be in not_dupl[]-meaning it is a repeating number
+        #if x is a repeating number
         if x in not_dupl:
-            # pop it at it's index
-            not_dupl.pop(not_dupl.index(x))
-
+             #check if x should be in not_dupl[]-meaning it is a repeating number
+            not_dupl.pop(not_dupl.index(x))   
         else:
             # If a non repeating number is found append it to not_dupl[]
             not_dupl.append(x)
-
-    return not_dupl
-
-
+    
+    # the return is inside an array. 
+    # how to convert to standalone integer? sum
+    return sum(not_dupl)
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
