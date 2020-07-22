c5246c287884c024b10b5a15513aa2cf363d86ef
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 16:08:29 -0400

    moving_zeroes passes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,15 +4,25 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-    # shift 0's to the left side of the array
+    # shift 0's to the end of the array
     # return the altered array
+
     # if there are no 0s print the array
+    
     # create a variable to hold the non-zeroes
+    moved = 0
     # Loop - for x in the range of the length of the array
+    for x in range(len(arr)):
         # x holds the zeroes in the equation, so if x doesnt equal zero and moved DOES equal zero
-        # the array's order becomes the moved nonzero number to the left, and the zero to the right for the entire array
-        # if moved didn't equal zero, move the index to check the next one
-pass
+        if arr[x] != 0 and arr[moved] == 0:
+            # the array's order gets swapped
+            arr[x], arr[moved] = arr[moved], arr[x]
+
+        if arr[moved] !=0:
+            # if moved didn't equal zero, move the index to check the next one
+            moved +=1
+
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
