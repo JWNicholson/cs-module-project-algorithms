26ad07d037dfd876b61b1948e5df50774ac3fc84
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 15:11:16 -0400

    working on single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,21 +5,25 @@ Returns: an integer
 def single_number(arr):
     # Your code here
     # Need to check an array for number that only occurs once while the rest show up twice
+    # Does array actually have duplicates? use set()??
     # How to look through an array to check if there is an integer that does NOT show up twice(loop??)
-    #If a single itnerger is in the array return that integer into a new array??
+    #If a single itnerger is in the array return that integer into a new array?? 
     #Else continue looping through the array until it is found
+
+
 ################
     #size = len(arr)
     #create a new array to find x
     not_dupl = []
-    
+    #loop through array
     for x in arr:
-        #check if x should be in not_dupl[]
+        #check if x should be in not_dupl[]-meaning it is a repeating number
         if x in not_dupl:
             # pop it at it's index
             not_dupl.pop(not_dupl.index(x))
 
         else:
+            # If a non repeating number is found append it to not_dupl[]
             not_dupl.append(x)
 
     return not_dupl
