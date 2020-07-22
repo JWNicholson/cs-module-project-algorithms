f72b776f9d046ee9ad28d7028104fc679d2f16a0
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 17:08:53 -0400

    eating_cookies passes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,25 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+#def eating_cookies(n):
     # Your code here
+def eating_cookies(n, cookie_jar=None):
+    
+    if cookie_jar == None:
+        cookie_jar = [0] * (n + 1)
 
-    pass
+    if n <= 1:
+        cookie_jar[n] =1
+
+    elif n == 2:
+        cookie_jar[n] = 2
+
+    elif cookie_jar[n] == 0:
+        cookie_jar[n] = eating_cookies(n - 1, cookie_jar) + eating_cookies(n - 2, cookie_jar) + eating_cookies(n - 3, cookie_jar)
+
+    return cookie_jar[n]
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
