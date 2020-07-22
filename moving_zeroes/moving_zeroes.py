61a1d25a873d8ae3ca886bc2965234d37e654af7
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 15:48:45 -0400

    planned out moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -12,7 +12,7 @@ def moving_zeroes(arr):
         # x holds the zeroes in the equation, so if x doesnt equal zero and moved DOES equal zero
         # the array's order becomes the moved nonzero number to the left, and the zero to the right for the entire array
         # if moved didn't equal zero, move the index to check the next one
-
+pass
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
